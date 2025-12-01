1576650a0d4ac96977138a034b5cd540f3c78e96
Anastasiia <sulollari.anastasiia@gmail.com>
2025-12-01 14:22:06 +0100

    Fix database connection

--- src/routes/mainRouter.ts
+++
@@ -1,8 +0,0 @@
-// import { Router } from "express";
-// import mainPageController from "../controllers/mainPageController.js";
-
-// const mainRouter = Router();
-
-// mainRouter.get("/", (req, res) => mainPageController.getMainPage(req, res));
-
-// export default mainRouter;
